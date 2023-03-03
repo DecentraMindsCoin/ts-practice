0648fe5c8f638d2c0969906b15568e88e8e3a63d
DecentraMindsCoin <decentraminds@gmail.com>
2023-03-02 23:55:52 -0800

    add todo list component with types

---
+++ src/components/TodoList.tsx
@@ -0,0 +1,92 @@
+import React, { FC, ChangeEvent, useState } from "react";
+
+export interface ITask {
+  taskName: string;
+  deadline: number;
+}
+export interface Props {
+  task: ITask;
+  completeTask(taskNameToDelete: string): void;
+}
+
+const TodoTask = ({ task, completeTask }: Props) => {
+  return (
+    <div className="task">
+      <div className="content">
+        <span>{task.taskName}</span>
+        <span>{task.deadline}</span>
+      </div>
+      <button
+        onClick={() => {
+          completeTask(task.taskName);
+        }}
+      >
+        X
+      </button>
+    </div>
+  );
+};
+
+const TodoList: FC = () => {
+  const [task, setTask] = useState<string>("");
+  const [deadLine, setDeadLine] = useState<number>(0);
+  const [todo, setTodo] = useState<ITask[]>([]);
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>): void => {
+    if (event.target.name === "task") {
+      setTask(event.target.value);
+    } else {
+      setDeadLine(Number(event.target.value));
+    }
+  };
+
+  const addTask = (): void => {
+    const newTask = {
+      taskName: task,
+      deadline: deadLine,
+    };
+    setTodo([...todo, newTask]);
+    setTask("");
+    setDeadLine(0);
+  };
+
+  const completeTask = (taskNameToDelete: string): void => {
+    setTodo(
+      todo.filter((task) => {
+        return task.taskName != taskNameToDelete;
+      })
+    );
+  };
+
+  return (
+    <div className="App">
+      <div className="header">
+        <div className="inputContainer">
+          <input
+            type="text"
+            placeholder="Task..."
+            name="task"
+            value={task}
+            onChange={handleChange}
+          />
+
+          <input
+            type="number"
+            placeholder="DeadLine (in days)"
+            name="deadline"
+            value={deadLine}
+            onChange={handleChange}
+          />
+        </div>
+        <button onClick={addTask}>Add Task</button>
+      </div>
+      <div className="todoList">
+        {todo.map((task: ITask, key: number) => {
+          return <TodoTask key={key} task={task} completeTask={completeTask} />;
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default TodoList;
